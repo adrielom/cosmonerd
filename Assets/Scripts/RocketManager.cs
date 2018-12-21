06c2fe589191c7ef5af125696db9110a0293e92f
Adriel <adriel.oliveira@dellead.com>
2018-12-21 20:30:34 -0300

    Scale Devices + Rocket Movement

---
+++ Assets/Scripts/RocketManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketManager : MonoBehaviour
+{
+    public float speed;
+
+    void OnMouseDrag()
+    {
+        MoveRocket();
+    }
+
+    public void MoveRocket()
+    {
+
+        //Drag Rocket
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, transform.position.y, 0);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        objPosition.y = transform.position.y;
+        objPosition.z = 0;
+        objPosition.x = Mathf.Clamp(objPosition.x, -2.5f, 2.5f);
+
+        transform.position = Vector3.MoveTowards(transform.position, objPosition, speed * Time.deltaTime);
+
+    }
+}
