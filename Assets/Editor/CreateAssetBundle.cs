c3a73731806d62b2e4c2a2005029cb7c30666be4
Adriel <adriel.oliveira@dellead.com>
2018-12-30 18:11:06 -0300

    AssetBundles working properly

--- Assets/Editor/CreateAssetBundle.cs
+++ Assets/Editor/CreateAssetBundle.cs
@@ -6,17 +6,17 @@ public class CreateAssetBundles
     [MenuItem("Assets/Build AssetBundles")]
     static void BuildAllAssetBundles()
     {
-        string assetBundleDirectory = "Assets/AssetBundles";
-        if(!Directory.Exists(assetBundleDirectory))
-		{
-			Directory.CreateDirectory(assetBundleDirectory);
-		}
-		#if UNITY_STANDALONE
+        string assetBundleDirectory = "Assets/StreamingAssets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+#if UNITY_STANDALONE
 			BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
-		#endif
+#endif
 
-		#if UNITY_ANDROID
-			BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
-		#endif
+#if UNITY_ANDROID
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
+#endif
     }
 }
---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class LoadAssetBundle : MonoBehaviour
+{
+    public string assetBundle;
+    string[] objects;
+    void Awake()
+    {
+        string assetBundleDirectory = Application.streamingAssetsPath + "/AssetBundles";
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(assetBundleDirectory, assetBundle));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        print(myLoadedAssetBundle.GetAllAssetNames());
+        objects = myLoadedAssetBundle.GetAllAssetNames();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            var prefab = myLoadedAssetBundle.LoadAsset<GameObject>(objects[i]);
+            Instantiate(prefab);
+        }
+
+        myLoadedAssetBundle.Unload(false);
+    }
+}
